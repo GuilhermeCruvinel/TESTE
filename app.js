3c4ac387fa5bc15390f1afd7e53d14df3bac3ce0
GuilhermeCruvinel <guilhermecruvineln@hotmail.com>
2024-04-12 11:37:46 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+alert(`ola mundo!`);
+let numeroSecreto = 4;
+console.log (`o numero secreto eh ${numeroSecreto}`);
+let chute;
+let tentativas = 1;
+while(chute != numeroSecreto) { 
+    chute=prompt(`escolha um numero de 1 a 10`);
+
+    if(chute == numeroSecreto) {;
+break;}
+    else {
+    if(chute > numeroSecreto) {alert(` O numero secredo e menor do que o seu ${chute}`);}
+else{
+    alert( `o numero secreto eh maior do que ${chute}`)
+ }
+tentativas++
+}
+}
+if (tentativas>1){alert(`vc acertou o numero secreto com ${tentativas} tentativas`)
+}else{alert(`vc acertou o numero secreto com ${tentativas} tentativa`);}
